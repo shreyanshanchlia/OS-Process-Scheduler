49b8890a628b011d3c9197155b819063bec87f0c
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-03-13 00:43:55 +0530

    added themes
    dark mode and light mode support

---
+++ Assets/src/Themes_image.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+public class Themes_image : MonoBehaviour
+{
+    public Sprite dark_image, light_image;
+    public Image refImage;
+    [Header("theme for color")]
+    public bool useColor;
+    public Color32 dark_color = new Color32(32,32,32,255), light_color = new Color32(223, 223, 223, 255);
+    void Start()
+    {
+        changeTheme();
+    }
+
+    void changeTheme()
+    {
+        int theme = PlayerPrefs.GetInt("theme", 0);
+        if (theme == 0) return;
+        if (!useColor)
+        {
+            changeThemeImage();
+        }
+        else
+        {
+            changeThemeColor();
+        }
+    }
+
+    private void changeThemeColor()
+    {
+        refImage.color = light_color;
+    }
+
+    void changeThemeImage()
+    {
+        refImage.sprite = light_image;
+    }
+}
