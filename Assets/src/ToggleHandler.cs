933499a2a4c5f9018a15edb1d0452808b2979bdf
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-03-13 04:34:02 +0530

    Preemptive toggle added

---
+++ Assets/src/ToggleHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+public class ToggleHandler : MonoBehaviour
+{
+    public GameObject toToggle;
+    public void toggleOff(bool OffSwitch)
+    {
+        print(!OffSwitch);
+        toToggle.SetActive(!OffSwitch);
+    }
+}
