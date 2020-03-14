626536773ec58702b80d0777a5a822a4165030d3
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-03-14 19:26:42 +0530

    Process Data storage
    Process now stores data in local fields to be accessed later.

--- Assets/src/PropertiesData.cs
+++ Assets/src/PropertiesData.cs
@@ -6,4 +6,21 @@ public class PropertiesData : MonoBehaviour
     public int ArrivalTime = 0;
     public int BurstTime = 1;
     public int Priority = 0;
+
+    public void UpdateProcessName(string _ProcessName)
+    {
+        ProcessName = _ProcessName;
+    }
+    public void UpdateArrivalTime(string _ArrivalTime)
+    {
+        ArrivalTime = int.Parse(_ArrivalTime);
+    }
+    public void UpdateBurstTime(string _BurstTime)
+    {
+        BurstTime = int.Parse(_BurstTime);
+    }
+    public void UpdatePriority(string _Priority)
+    {
+        Priority = int.Parse(_Priority);
+    }
 }
