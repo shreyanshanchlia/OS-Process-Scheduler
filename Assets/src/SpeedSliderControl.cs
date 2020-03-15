9b5e56b4072d7240a1ea364f78f78858a44aade5
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-03-15 05:42:00 +0530

    Speed Control
    Speed can be seen and edited at runtime, which have been locked at particular intervals.

---
+++ Assets/src/SpeedSliderControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using TMPro;
+public class SpeedSliderControl : MonoBehaviour
+{
+    [ReadOnly] public float speed = 1.0f;
+    public TMP_InputField speedText;
+    public void Adjust(float value)
+    {
+        if (value == 0)
+        {
+            speed = 0.0f;
+        }
+        else if (value <= 10)
+        {
+            speed = value / 10;
+        }
+        else
+        {
+            speed = value - 10;
+        }
+        RefereshSpeed();
+    }
+    public void RefereshSpeed()
+    {
+        Time.timeScale = speed;
+        speedText.text = speed.ToString();
+    }
+}
