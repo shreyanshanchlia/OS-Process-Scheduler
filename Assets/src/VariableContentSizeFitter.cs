d36cc4778417e6b203b6dedc7f59735cf9645335
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-03-15 09:57:00 +0530

    scaling issue fix
    Scaling issue when process was expanded but scale was not being calculated fixed.

--- Assets/src/VariableContentSizeFitter.cs
+++ Assets/src/VariableContentSizeFitter.cs
@@ -15,7 +15,7 @@ public class VariableContentSizeFitter : MonoBehaviour
         AdditionalOffset = 0;
         foreach (RectTransform rectTransform in childRectTransform)
         {
-            if(rectTransform.gameObject.activeSelf)
+            if (rectTransform.gameObject != null && rectTransform.gameObject.activeSelf)
                 AdditionalOffset += rectTransform.sizeDelta.y;
         }
         ToHandleRectTransform.sizeDelta = new Vector2(ToHandleRectTransform.sizeDelta.x, AdditionalOffset);
