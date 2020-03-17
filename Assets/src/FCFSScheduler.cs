770c901a1d3516776ca6c71fd921927621906b6a
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-03-17 18:39:46 +0530

    Error fixes and improvements
    Collection was modified, enumeration operation may not execute fixed. Changed from list to Queue.

--- Assets/src/FCFSScheduler.cs
+++ Assets/src/FCFSScheduler.cs
@@ -5,7 +5,7 @@ public class FCFSScheduler : MonoBehaviour
 {
     public Scheduler scheduler;
     public ChartMaker chartMaker;
-    private List<PropertiesData> arrived;
+    private Queue<PropertiesData> arrived;
     private List<PropertiesData> waiting;
     bool running = false;
     bool processing = false;
@@ -14,7 +14,7 @@ public class FCFSScheduler : MonoBehaviour
     public void run()
     {
         waiting = new List<PropertiesData>();
-        arrived = new List<PropertiesData>();
+        arrived = new Queue<PropertiesData>();
         foreach (var _process in scheduler.ProcessList)
         {
             waiting.Add(_process);
@@ -25,20 +25,20 @@ public class FCFSScheduler : MonoBehaviour
     {
         if (running)
         {
-            foreach (PropertiesData propertiesData in waiting)
+            for (int i = 0; i < waiting.Count; i++)
             {
+                PropertiesData propertiesData = waiting[i];
                 if (scheduler.SchedulerTime > propertiesData.ArrivalTime)
                 {
-                    arrived.Add(propertiesData);
+                    arrived.Enqueue(propertiesData);
                     waiting.Remove(propertiesData);
                 }
             }
             if(!processing)
             {
                 if (arrived.Count > 0)
-                {
-                    CurrentlyProcessing = arrived[0];
-                    arrived.RemoveAt(0);
+                { 
+                    CurrentlyProcessing = arrived.Dequeue();
                     processing = true;
                     ProcessorFreeAt = CurrentlyProcessing.BurstTime;
                     chartMaker.GenerateChartElement(CurrentlyProcessing.ProcessName, scheduler.SchedulerTime);
