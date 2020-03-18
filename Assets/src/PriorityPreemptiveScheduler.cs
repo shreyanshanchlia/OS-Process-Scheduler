500eb82cfb12188c41f8686dae4bf3b1a4263585
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-03-19 00:00:38 +0530

    Rename
    Renamed file for better consistancy.

--- Assets/src/PriorityPreemptiveScheduler.cs
+++ Assets/src/PriorityPreemptiveScheduler.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class PrioritySchedulerPreemptive : MonoBehaviour
+public class PriorityPreemptiveScheduler : MonoBehaviour
 {
     public Scheduler scheduler;
     public ChartMaker chartMaker;
