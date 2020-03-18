c563f41572bbc4e9798d1ce1997ab7ccf4e63678
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-03-19 02:07:11 +0530

    tabs can be added and closed

---
+++ Assets/src/TabManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TabManager : MonoBehaviour
+{
+    public GameObject LinkedTab;
+    public void CloseTab()
+    {
+        Destroy(LinkedTab);
+        Destroy(this.gameObject);
+    }
+    public void OpenTab()
+    {
+        LinkedTab.GetComponent<RectTransform>().SetAsLastSibling();
+    }
+}
--- Assets/src/TabsManager.cs
+++ Assets/src/TabsManager.cs
@@ -3,10 +3,18 @@ using UnityEngine.UI;
 public class TabsManager : MonoBehaviour
 {
     public RectTransform WindowLauncher_RectTransform;
-
+    public GameObject MainTabPrefab;
+    public GameObject TabsHolder;
+    public GameObject ButtonTabPrefab;
+    public GameObject ButtonTabHolder;
     void Start()
     {
         WindowLauncher_RectTransform.SetAsLastSibling();
     }
-
+    public void AddTab()
+    {
+        GameObject @object = Instantiate(MainTabPrefab, TabsHolder.transform);
+        GameObject tab = Instantiate(ButtonTabPrefab, ButtonTabHolder.transform);
+        tab.GetComponent<TabManager>().LinkedTab = @object;
+    }
 }
