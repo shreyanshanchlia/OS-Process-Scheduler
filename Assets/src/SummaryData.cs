cbfafb539db9aca25d4a152b6e8e3f9ec77ff0dd
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-03-19 08:02:57 +0530

    priority column added to summary

--- Assets/src/SummaryData.cs
+++ Assets/src/SummaryData.cs
@@ -4,6 +4,7 @@ public class SummaryData
     public string ProcessName;
     public int ArrivalTime;
     public int BurstTime;
+    public int priority;
     public float CompilationTime;
     public float TurnAroundTime;
     public float WaitingTime;
