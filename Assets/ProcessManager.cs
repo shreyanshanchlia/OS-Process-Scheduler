862f6da38d13a8afca47b55b80d130808a2e39f8
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-03-19 06:56:24 +0530

    Prefab Update

---
+++ Assets/ProcessManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ProcessManager : MonoBehaviour
+{
+    public GameObject Processes;
+    public void ResetPage()
+    {
+        foreach(Transform process in Processes.transform)
+        {
+            Destroy(process.gameObject);
+        }
+    }
+}
