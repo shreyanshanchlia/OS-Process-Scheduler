04d3a27cf51fa3ad757c8caab7568f1e895309dd
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-04-02 16:30:28 +0530

    Update SJFPreemptiveScheduler.cs

--- Assets/src/SJFPreemptiveScheduler.cs
+++ Assets/src/SJFPreemptiveScheduler.cs
@@ -54,6 +54,7 @@ public class SJFPreemptiveScheduler : MonoBehaviour
     {
         if (!running)
         {
+            reset();
             InitializeWaitingList();
             running = true;
         }
