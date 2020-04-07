cb1f755f78c2aea8d10d2c659999c438546feb78
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-04-07 13:51:26 +0530

    System Info
    Added System Info on launch

---
+++ Assets/src/LoadSystemInformation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class LoadSystemInformation : MonoBehaviour
+{
+    public TextMeshProUGUI ProcessorInfoText;
+    private void Start()
+    {
+        StartCoroutine(LoadProcessor());
+    }
+    IEnumerator LoadProcessor()
+    {
+        yield return new WaitForSeconds(1.0f);
+        ProcessorInfoText.text = "Using " + SystemInfo.processorType;
+    }
+}
