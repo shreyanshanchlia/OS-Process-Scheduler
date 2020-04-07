7c10da4db1c2f19b0886c00bba6c8eba911eb99d
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-04-07 21:21:46 +0530

    open Settings  button

---
+++ Assets/src/OpenMainSettings.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class OpenMainSettings : MonoBehaviour
+{
+    [ReadOnly] public GameObject mainSettings;
+    private void Start()
+    {
+        mainSettings = GameObject.FindGameObjectWithTag("Settings");
+    }
+    public void OpenSettings()
+    {
+        mainSettings?.GetComponent<RectTransform>().SetAsLastSibling();        
+    }
+}
