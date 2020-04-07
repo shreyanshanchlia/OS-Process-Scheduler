a2add7839b17099cfff58f81607033ee96005a62
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-04-07 18:21:32 +0530

    Text Changing on toggle
    When toggle is switched, the relative text will now change.

--- Assets/src/ToggleHandler.cs
+++ Assets/src/ToggleHandler.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 public class ToggleHandler : MonoBehaviour
 {
     public GameObject toToggle;
+    public TextMeshProUGUI ToggleText;
+    public string Offstring, Onstring;
     public void toggleOff(bool OffSwitch)
     {
         toToggle.SetActive(!OffSwitch);
+        if(OffSwitch)
+        {
+            ToggleText.text = Onstring;
+        }
+        else
+        {
+            ToggleText.text = Offstring;
+        }
     }
 }
