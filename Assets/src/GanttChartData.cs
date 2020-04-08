686d3efe2ac30114802f39cbbe1469eb624aca2d
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-04-08 23:25:35 +0530

    Summary Gantt Chart UI Added

---
+++ Assets/src/GanttChartData.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class GanttChartData : MonoBehaviour
+{
+    public string ProcessName;
+    public Sprite ProcessingPos;
+}
---
+++ Assets/src/GanttChartSummaryManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GanttChartSummaryManager : MonoBehaviour
+{
+    public List<GanttChartData> DetailedGanttChart;
+
+    
+}
