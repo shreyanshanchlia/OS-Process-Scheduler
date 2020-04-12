ee46baafddab897875add74c611ea7b27fdbb29d
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-04-13 03:19:04 +0530

    Switching between Summaries
    Added Mechanism for switching between summaries

---
+++ Assets/ToggleSummaries.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using TMPro;
+public class ToggleSummaries : MonoBehaviour
+{
+    [System.Serializable]
+    public struct SummaryHolder
+    {
+        public GameObject Summary;
+        public string ButtonName;
+    }
+    public SummaryHolder[] Summaries;
+    public TextMeshProUGUI SummaryButtonName;
+    private int currentSummary = 0;
+
+    public void Toggle()
+    {
+        for (int i = 0; i < Summaries.Length; i++)
+        {
+            Summaries[i].Summary.SetActive(false);
+        }
+        Summaries[(++currentSummary) % Summaries.Length].Summary.SetActive(true);
+        SummaryButtonName.text = Summaries[(currentSummary) % Summaries.Length].ButtonName;
+    }
+}
